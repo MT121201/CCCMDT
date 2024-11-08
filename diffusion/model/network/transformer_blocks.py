383b94776d584b10c399d5075770c1c77d11516a
MT121201 <triet.kieu_binu@hcmut.edu.vn>
2024-11-08 17:42:08 +0800

    DiT_model

---
+++ diffusion/model/network/transformer_blocks.py
@@ -0,0 +1,10 @@
+# References:
+# Meta: https://github.com/PixArt-alpha/PixArt-alpha
+# GLIDE: https://github.com/openai/glide-text2im
+# MAE: https://github.com/facebookresearch/mae/blob/main/models_mae.py
+
+import torch
+import torch.nn as nn
+
+
+# class MutiHeadCrossAttention
